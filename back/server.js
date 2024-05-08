3abffd70f30482db245e54742140afd22fd51c46
ryan harrelson <lostramen1998@gmail.com>
2024-05-07 23:07:53 -0500

    deployment

--- back/server.js
+++ back/server.js
@@ -5,12 +5,14 @@ import messageroutes from "./routes/message.routes.js";
 import connectDB from "./db/dbConnect.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
-import cors from "cors";
+import path from "path";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
 const Port = process.env.Port || 5000;
+
+const ___dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
@@ -20,6 +22,12 @@ connectDB();
 app.use("/api/auth", authroutes);
 app.use("/api/messages", messageroutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(___dirname, "/front/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(___dirname, "front", "dist", "index.html"));
+});
 
 server.listen(Port, () =>
   console.log("server is runnin at http://localhost:5000")
